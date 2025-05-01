15fabab98d1cb36142063c88955065b4358e170e
Gorsky Kirill <kirillgorskiy@gmail.com>
2025-05-01 12:57:42 +0300

    codestyle + better CMakeLists.txt

--- src/solver.hpp
+++ src/solver.hpp
@@ -1,14 +1,15 @@
 #ifndef SOLVER_HPP
 #define SOLVER_HPP
 
+#include "acb_vector.hpp"
 #include <flint/acb.h>
+#include <flint/acb_calc.h>
 #include <flint/acb_mat.h>
 #include <flint/arb.h>
-#include <flint/acb_calc.h>
-#include "acb_vector.hpp"
 
-void make_matrix(acb_mat_t& matrix, slong matrix_size, acb_vector& zeros, slong precision);
+void make_matrix(acb_mat_t &matrix, slong matrix_size, acb_vector &zeros,
+                 slong precision);
 
-void solve(acb_mat_t& a, slong n, slong precision);
+void solve(acb_mat_t &a, slong n, slong precision);
 
-#endif 
+#endif
